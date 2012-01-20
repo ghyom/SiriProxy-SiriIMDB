ac74cb3d0e3090c395fcf287e5ffdac7a8128fd9
Ronan Pozzi <contact@treenity-web.fr>
2012-01-20 23:24:27 +0100

    updated

--- lib/siriproxy-siriimdb.rb
+++ lib/siriproxy-siriimdb.rb
@@ -33,7 +33,7 @@ class SiriProxy::Plugin::SiriIMDB < SiriProxy::Plugin
 	return movieRating
   end
   
-  listen_for /(Dois-je regarder|Dois-je voir|Que penses-tu de|Est-ce que tu me conseilles) (.*)/i do |question, movieTitle|
+  listen_for /(Dois-je regarder|Dois-je voir|Que penses-tu de|Est-ce que tu me conseilles|Est-ce que tu me conseil) (.*)/i do |question, movieTitle|
 	movieTitle = movieTitle.split(' ').map {|w| w.capitalize }.join(' ')
 	movieRating = getRating(movieTitle)
 	movieRatingString = movieRating.to_s
@@ -72,7 +72,7 @@ class SiriProxy::Plugin::SiriIMDB < SiriProxy::Plugin
 	  request_completed
   end
   
-  listen_for /Quand est sortit (.*)/i do |movieTitle|
+  listen_for /(Quand est sortit|Quand a été produit|Quand a été réalisés|Quand a été réalisé) (.*)/i do |question, movieTitle|
 	movieTitle = movieTitle.split(' ').map {|w| w.capitalize }.join(' ')
 	search = Imdb::Search.new(movieTitle)
 	movie = search.movies[0]
